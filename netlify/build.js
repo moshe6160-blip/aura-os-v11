c5fb6280964fc8a039138128c3f7828b5d7de3b6
moshe6160-blip <moshe6160@gmail.com>
2026-06-13 08:54:05 +0200

    Add files via upload

--- netlify/build.js
+++ netlify/build.js
@@ -1,5 +1,1 @@
-const fs=require('fs');
-fs.rmSync('dist',{recursive:true,force:true});
-fs.mkdirSync('dist',{recursive:true});
-fs.copyFileSync('index.html','dist/index.html');
-console.log('AURA OS build complete');
+const fs=require('fs');fs.rmSync('dist',{recursive:true,force:true});fs.mkdirSync('dist',{recursive:true});fs.copyFileSync('index.html','dist/index.html');console.log('AURA easy build complete');
